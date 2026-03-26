3bca1477b5607f89bd918a593c60e3e21c6ca0b1
hahafiz <hafizqamarul19@gmail.com>
2026-03-26 21:49:16 +0800

    progress

--- src/components/screen/gameplay.tsx
+++ src/components/screen/gameplay.tsx
@@ -1,5 +1,12 @@
 // TODO: gameplay screen
+import { useState } from "react";
 
 export function Gameplay() {
-  return <div>Gameplay</div>;
+  const [backgroundColor, setBackgroundColor] = useState("#808080");
+
+  return (
+    <div className="h-screen py-16 px-8" style={{ backgroundColor }}>
+      <div className="bg-slate-50 p-2 px-8 rounded-full text-xl">CSSGuessr</div>
+    </div>
+  );
 }
--- src/types/game.ts
+++ src/types/game.ts
@@ -1,1 +1,12 @@
 // color, score, gamestate interfaces
+
+export interface Color {
+  r: number;
+  g: number;
+  b: number;
+  a?: number;
+}
+
+export interface Round {
+  round: number;
+}
