5bbf392f6514b48ef53c067af204315aec2024f3
hahafiz <hafizqamarul19@gmail.com>
2026-03-26 21:55:46 +0800

    fix game state type

--- src/types/game.ts
+++ src/types/game.ts
@@ -1,12 +1,8 @@
 // color, score, gamestate interfaces
 
-export interface Color {
-  r: number;
-  g: number;
-  b: number;
-  a?: number;
-}
-
-export interface Round {
+export interface GameState {
+  user: string;
+  color: string;
+  score: number;
   round: number;
 }
