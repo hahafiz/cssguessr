945f5caede2578ac9961fddf5b87b67f17e2a01e
hahafiz <hafizqamarul19@gmail.com>
2026-03-30 21:22:11 +0800

    added random color generator logic

--- src/components/screen/gameplay.tsx
+++ src/components/screen/gameplay.tsx
@@ -1,12 +1,20 @@
 // TODO: gameplay screen
 import { useState } from "react";
+import { type GameState } from "../../types/game";
+import { generateRandomHex } from "../../lib/color-generator";
 
-export function Gameplay() {
+export function Gameplay({ user, color, score, round }: GameState) {
   const [backgroundColor, setBackgroundColor] = useState("#808080");
+
+  const handleColorChange = () => {
+    const newColor = generateRandomHex();
+    setBackgroundColor(newColor);
+  };
 
   return (
     <div className="h-screen py-16 px-8" style={{ backgroundColor }}>
       <div className="bg-slate-50 p-2 px-8 rounded-full text-xl">CSSGuessr</div>
+      <button onClick={handleColorChange}>Change Color</button>
     </div>
   );
 }
--- src/lib/color-generator.ts
+++ src/lib/color-generator.ts
@@ -1,1 +1,10 @@
 // random color generation logic
+export const generateRandomHex = () => {
+  const randomColor =
+    "#" +
+    Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, "0"); // ensure color is 6 characters long
+
+  return randomColor;
+};
