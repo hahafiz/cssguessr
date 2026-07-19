1ddbcf5290208e883f58d0d08597a8a59263d2e6
hahafiz <hafizqamarul19@gmail.com>
2026-07-19 20:05:29 +0800

    test: added row to table rooms

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import db from "./database.ts";
+import crypto from "crypto";
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -14,5 +15,38 @@ app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
 
-const query = db.prepare("SELECT color_sequence FROM rooms");
+// get all table list
+const query = db.prepare("SELECT name FROM sqlite_master WHERE type='table'");
 console.log("DATABASE: ", query.all());
+
+// insert a row to ROOMS table
+const colorSequence = ["#ff0000", "#00ff00", "#0000ff"];
+const colorJsonStr = JSON.stringify(colorSequence);
+const roomId = crypto.randomUUID();
+
+const insert = db.prepare(
+  "INSERT INTO rooms (id, color_sequence, max_players) values (?, ?, ?)",
+);
+
+// log insert
+const result = insert.run(roomId, colorJsonStr, 4);
+
+console.log("--- INSERT SUCCESSFUL");
+console.log(`Last inserted ID: ${result.lastInsertRowid}`);
+console.log(`Room string ID created: ${roomId}`);
+console.log(`Changes made: ${result.changes}`);
+
+// log all rows
+const rowQuery = db.prepare("SELECT * FROM rooms");
+const allRow = rowQuery.all();
+
+console.log("--- ALL ROOMS ---");
+allRow.forEach((room: any) => {
+  const colors = JSON.parse(room.color_sequence);
+
+  console.log(`Room ID: ${room.id}`);
+  console.log(`Max_players: ${room.max_players}`);
+  console.log(`Colors: ${room.color_sequence}`);
+  console.log(`Status: ${room.status}`);
+  console.log("----------------------");
+});
