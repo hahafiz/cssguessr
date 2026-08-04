45e1310c4bea6c9ecef02f95d33fa98405ed1783
hahafiz <hafizqamarul19@gmail.com>
2026-08-04 23:56:12 +0800

    generaterawcolorsequence done

--- apps/server/src/utils/colors.ts
+++ apps/server/src/utils/colors.ts
@@ -7,26 +7,32 @@ function getRandomInt(min: number, max: number): number {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function hslToRgb(color: RawColor): [number, number, number];
-function formatAsHsl(color: RawColor): string; // uses color directly
-function formatAsRgb(color: RawColor): string; // calls hsltorgb first
+function hslToRgb(color: RawColor): [number, number, number] {}
+function formatAsHsl(color: RawColor): string {} // uses color directly
+function formatAsRgb(color: RawColor): string {} // calls hsltorgb first
 
-function formatColor(color: RawColor, format: ColorFormat): string;
+function formatColor(color: RawColor, format: ColorFormat): string {
+  return {
+    hsl: formatAsHsl(color),
+    rgb: formatAsRgb(color),
+  };
+}
 
 function generateRawColorSequence(): RawColor[] {
-  const colorSequence = new Array<string>(SEQUENCE_LENGTH);
+  const colorSequence = new Array<RawColor>(SEQUENCE_LENGTH);
 
   for (let i = 0; i < SEQUENCE_LENGTH; i++) {
     const hue = getRandomInt(0, 360);
     const saturation = getRandomInt(0, 100);
     const lightness = getRandomInt(0, 100);
 
-    const color: string = `${hue} ${saturation} ${lightness}`;
-    colorSequence[i] = color;
+    colorSequence[i] = [hue, saturation, lightness];
   }
+
+  return colorSequence;
 }
 
 export function formatColorSequence(
   colors: RawColor[],
   format: ColorFormat,
-): string[];
+): string[] {}
