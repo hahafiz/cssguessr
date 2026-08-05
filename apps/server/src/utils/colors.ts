ecf92fe140327ff4e70341df8c5008dfc8a9389c
hahafiz <hafizqamarul19@gmail.com>
2026-08-05 13:30:06 +0800

    hsltorgb function done

--- apps/server/src/utils/colors.ts
+++ apps/server/src/utils/colors.ts
@@ -7,14 +7,72 @@ function getRandomInt(min: number, max: number): number {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function generateRawColorSequence(): RawColor[] {
+  const colorSequence = new Array<RawColor>(SEQUENCE_LENGTH);
+
+  for (let i = 0; i < SEQUENCE_LENGTH; i++) {
+    const hue = getRandomInt(0, 359);
+    const saturation = getRandomInt(0, 100);
+    const lightness = getRandomInt(0, 100);
+
+    colorSequence[i] = [hue, saturation, lightness];
+  }
+
+  return colorSequence;
+}
+
 function hslToRgb(color: RawColor): [number, number, number] {
   const [hue, saturation, lightness] = color;
+  const h = Math.floor(hue / 60);
   const s = saturation / 100;
   const l = lightness / 100;
   const chroma = (1 - Math.abs(2 * l - 1)) * s;
-
   const x = chroma * (1 - Math.abs(((hue / 60) % 2) - 1));
+  const m = l - chroma / 2; // brightness
+
+  let r, g, b;
+  switch (h) {
+    case 0:
+      r = chroma;
+      g = x;
+      b = 0;
+      break;
+    case 1:
+      r = x;
+      g = chroma;
+      b = 0;
+      break;
+    case 2:
+      r = 0;
+      g = chroma;
+      b = x;
+      break;
+    case 3:
+      r = 0;
+      g = x;
+      b = chroma;
+      break;
+    case 4:
+      r = x;
+      g = 0;
+      b = chroma;
+      break;
+    case 5:
+      r = chroma;
+      g = 0;
+      b = x;
+      break;
+    default:
+      throw new Error(`Invalid hue sector: h=${h} from hue=${hue}`);
+  }
+
+  let red: number = Math.round((r + m) * 255);
+  let green: number = Math.round((g + m) * 255);
+  let blue: number = Math.round((b + m) * 255);
+
+  return [red, green, blue];
 }
+
 function formatAsHsl(color: RawColor): string {} // uses color directly
 function formatAsRgb(color: RawColor): string {} // calls hsltorgb first
 
@@ -23,20 +81,6 @@ function formatColor(color: RawColor, format: ColorFormat): string {
     hsl: formatAsHsl(color),
     rgb: formatAsRgb(color),
   };
-}
-
-function generateRawColorSequence(): RawColor[] {
-  const colorSequence = new Array<RawColor>(SEQUENCE_LENGTH);
-
-  for (let i = 0; i < SEQUENCE_LENGTH; i++) {
-    const hue = getRandomInt(0, 360);
-    const saturation = getRandomInt(0, 100);
-    const lightness = getRandomInt(0, 100);
-
-    colorSequence[i] = [hue, saturation, lightness];
-  }
-
-  return colorSequence;
 }
 
 export function formatColorSequence(
