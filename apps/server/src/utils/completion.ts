8d84284526494ecb2cffeeb38e92e3daeeac32cc
hahafiz <hafizqamarul19@gmail.com>
2026-08-12 22:55:06 +0800

    progress

--- apps/server/src/utils/completion.ts
+++ apps/server/src/utils/completion.ts
@@ -1,4 +1,15 @@
 import db from "./../database.ts";
-const query = db.prepare(
-  "SELECT COUNT(DISTINCT player_id) AS player_count FROM scores WHERE room_id = ? AND (? IS NULL OR round_number = ?)",
+
+// find how many submitted scores done by different player_id for this room_id
+const scoreCountQuery = db.prepare(
+  "SELECT COUNT(DISTINCT player_id) AS player_countFROM scores WHERE room_id = ? AND (? IS NULL OR round_number = ?)",
 );
+
+const room_id = "test";
+const round_number = 2;
+
+// for a specific round:
+scoreCountQuery.get(room_id, round_number, round_number);
+
+// for whole game:
+scoreCountQuery.get(room_id, null, null);
