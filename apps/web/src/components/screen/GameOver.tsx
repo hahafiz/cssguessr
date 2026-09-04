158504a61fba372db2993eb17c0c3d7b3ba591c5
hahafiz <hafizqamarul19@gmail.com>
2026-09-04 21:39:13 +0800

    progress

--- apps/web/src/components/screen/GameOver.tsx
+++ apps/web/src/components/screen/GameOver.tsx
@@ -2,6 +2,24 @@
 // TODO: reset button
 // TODO: share button
 
-export function GameOver() {
+import { useEffect, useState } from "react";
+import { getResults } from "../../api/rooms";\
+
+interface GameOverProps {
+  roomId: string;
+  playerId: string
+}
+
+export function GameOver({ roomId, playerId } : GameOverProps) {
+  const [result, setResult] = useState();
+
+  useEffect(() => {
+    const fetchResult = async () => {
+      const res = await getResults(roomId);
+      setResult(res);
+    };
+    fetchResult();
+  }, [roomId]);
+
   return <p>Game Over screen here</p>;
 }
