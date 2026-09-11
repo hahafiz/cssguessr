87f8b11d7298938c0cae37e00937660a0a2cd6fe
hahafiz <hafizqamarul19@gmail.com>
2026-09-11 17:53:01 +0800

    gameoptions scaffold

--- apps/web/src/components/screen/GameOptions.tsx
+++ apps/web/src/components/screen/GameOptions.tsx
@@ -1,3 +1,6 @@
+import { useSearchParams } from "react-router";
 export default function GameOptions() {
+  const [searchParams] = useSearchParams();
+  const mode = searchParams.get("mode");
   return <>Test</>;
 }
