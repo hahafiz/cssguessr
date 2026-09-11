4fd85d32f6e1e3758cb2caef62a8da9134504f45
hahafiz <hafizqamarul19@gmail.com>
2026-09-12 00:47:24 +0800

    gameoptions now createRoom and will pass playerId to gameplay

--- apps/web/src/components/screen/GameOptions.tsx
+++ apps/web/src/components/screen/GameOptions.tsx
@@ -1,6 +1,19 @@
+import { useState, useEffect } from "react";
 import { useSearchParams } from "react-router";
+import type { RoomWithPlayer } from "@cssguessr/shared-types";
+import { createRoom } from "../../api/rooms";
+
 export default function GameOptions() {
+  const [room, setRoom] = useState<RoomWithPlayer | null>(null);
   const [searchParams] = useSearchParams();
   const mode = searchParams.get("mode");
+
+  useEffect(() => {
+    const fetchRoom = async () => {
+      const newRoom = await createRoom({ max_players: 1 });
+      setRoom(newRoom);
+    };
+    fetchRoom();
+  }, []);
   return <>Test</>;
 }
