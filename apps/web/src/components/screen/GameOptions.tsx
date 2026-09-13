099d272ec9d8b7b5a9739e339ba64f46f790bb60
hahafiz <hafizqamarul19@gmail.com>
2026-09-13 21:44:42 +0800

    GameOptions progress

--- apps/web/src/components/screen/GameOptions.tsx
+++ apps/web/src/components/screen/GameOptions.tsx
@@ -1,19 +1,93 @@
-import { useState, useEffect } from "react";
-import { useSearchParams } from "react-router";
-import type { RoomWithPlayer } from "@cssguessr/shared-types";
+import { useState } from "react";
+import { useNavigate, useSearchParams } from "react-router";
 import { createRoom } from "../../api/rooms";
+import { Button } from "../ui/button/Button";
+import { storePlayerId } from "../../api/playerId";
 
 export default function GameOptions() {
-  const [room, setRoom] = useState<RoomWithPlayer | null>(null);
   const [searchParams] = useSearchParams();
   const mode = searchParams.get("mode");
 
-  useEffect(() => {
-    const fetchRoom = async () => {
-      const newRoom = await createRoom({ max_players: 1 });
-      setRoom(newRoom);
-    };
-    fetchRoom();
-  }, []);
-  return <>Test</>;
+  const [maxPlayers, setMaxPlayers] = useState(2);
+  const [colorFormat, setColorFormat] = useState<"rgb" | "hsl">("rgb");
+  const [inputMethod, setInputMethod] = useState<"slider" | "hex">("slider");
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const navigate = useNavigate();
+
+  if (mode !== "solo" && mode !== "duel") {
+    return (
+      <p>
+        Invalid game mode
+        <Button onClick={() => navigate("/")}>Back to Menu</Button>
+      </p>
+    );
+  }
+
+  const onCreateGame = async () => {
+    setIsSubmitting(true);
+    try {
+      const newRoom = await createRoom({
+        max_players: mode === "duel" ? maxPlayers : 1,
+      });
+      storePlayerId(newRoom.id, newRoom.player_id);
+
+      if (mode === "duel") {
+        navigate(`/duel/lobby/${newRoom.id}`);
+      } else {
+        navigate(`/solo/${newRoom.id}`);
+      }
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
+  return (
+    <>
+      <div>
+        <p>Select color mode</p>
+        <div>
+          <Button variant="primary" onClick={() => setColorFormat("rgb")}>
+            RGB
+          </Button>
+          <Button
+            variant="primary"
+            onClick={() => setColorFormat("hsl")}
+            disabled
+          >
+            HSL
+          </Button>
+        </div>
+      </div>
+
+      {colorFormat === "rgb" && (
+        <div>
+          <p>Select RGB input format</p>
+          <div>
+            <Button variant="primary" value="slider">
+              Slider
+            </Button>
+            <Button variant="primary" value="hex" disabled>
+              Hex
+            </Button>
+          </div>
+        </div>
+      )}
+
+      {mode === "duel" && (
+        <>
+          <label>Max player</label>
+          <input
+            id="max-player"
+            type="number"
+            value={maxPlayers}
+            onChange={() => setMaxPlayers}
+          ></input>
+        </>
+      )}
+
+      <Button variant="primary" onClick={onCreateGame} disabled={isSubmitting}>
+        Start Game
+      </Button>
+    </>
+  );
 }
