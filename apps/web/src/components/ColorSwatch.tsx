4566d6489697f07b1eb48a9e4705d2191fe49f94
hahafiz <hafizqamarul19@gmail.com>
2026-09-18 17:10:26 +0800

    styling colorswatch

--- apps/web/src/components/ColorSwatch.tsx
+++ apps/web/src/components/ColorSwatch.tsx
@@ -4,6 +4,9 @@ interface ColorSwatchProps {
 
 export function ColorSwatch({ color }: ColorSwatchProps) {
   return (
-    <div className="h-screen py-16 px-8" style={{ backgroundColor: color }} />
+    <div
+      className="h-48 w-48 rounded-full p-2 outline-4 -outline-offset-5 outline-white/30"
+      style={{ backgroundColor: color }}
+    />
   );
 }
