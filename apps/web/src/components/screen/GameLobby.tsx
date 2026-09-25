17d49c54ebeb109376b362430fcccc2a68b358fd
hahafiz <hafizqamarul19@gmail.com>
2026-09-25 15:03:40 +0800

    join room logic done

--- apps/web/src/components/screen/GameLobby.tsx
+++ apps/web/src/components/screen/GameLobby.tsx
@@ -1,25 +1,40 @@
 import { useParams } from "react-router";
 import { Button } from "../ui/button/Button";
-import { getStoredPlayerId } from "../../api/playerId";
+import { getStoredPlayerId, storePlayerId } from "../../api/playerId";
 import { useEffect, useState } from "react";
-import { getRoom } from "../../api/rooms";
+import { getRoom, joinRoom } from "../../api/rooms";
 import type { Room } from "@cssguessr/shared-types";
 
 export default function GameLobby() {
   const [room, setRoom] = useState<Room | null>(null);
   const [roomStatus, setRoomStatus] = useState("waiting");
   const { roomId } = useParams();
-  const playerId = roomId ? getStoredPlayerId(roomId) : null;
+  const [playerId, setPlayerId] = useState<string | null>(() =>
+    roomId ? getStoredPlayerId(roomId) : null,
+  );
 
   useEffect(() => {
     const fetchRoom = async () => {
       if (roomId) {
-        const newRoom = await getRoom(roomId);
-        setRoom(newRoom);
+        if (!playerId) {
+          const newRoom = await joinRoom(roomId);
+          setRoom(newRoom);
+          storePlayerId(roomId, newRoom.player_id);
+          setPlayerId(newRoom.player_id);
+        } else {
+          const newRoom = await getRoom(roomId);
+          setRoom(newRoom);
+        }
       }
     };
     fetchRoom();
-  }, [roomId]);
+  }, [roomId, playerId]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {}, 3000);
+
+    return () => clearInterval(interval);
+  });
 
   return (
     <>
